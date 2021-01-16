2a381f228170f5afdb6824f24361fb6cf8dc611c
alstjd8826 <alstjd8826@github.com>
2021-01-16 17:19:31 +0900

    commit

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,10 @@ export const join = (req,res) => res.render('join', {pageTitle:"Join"});
 export const login = (req,res) => res.send('login');
 export const logout = (req,res) => res.send('logout');
 export const users = (req,res) => res.send('Users');
+export const editProfile = (req,res) => {
+    res.render('editprofile', {
+        pageTitle: "Edit Your Profile"
+    })
+};
 export const userDetail = (req,res) => res.send('User Detail');
-export const editProfile = (req,res) => res.send('Edit Profile');
 export const changePassword = (req,res) => res.send('Change Password');
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,28 @@
-export const home = (req,res) => res.render('home');
-export const search = (req,res) => res.send('Search');
-export const videos = (req,res) => res.send('Videos');
+import {
+    videos
+  } from "../db";
+   
+export const home = (req, res) => {
+    res.render("home", {
+      pageTitle: "Main",
+      videos //videos 라고만 써도 돼요!
+    });
+};
+export const search = (req,res) => {
+    const { 
+        query: { 
+            term: searchTerm 
+        }
+      } = req;
+      res.render("search", {
+        pageTitle: "Search",
+        searchTerm,
+        videos
+      });
+    };
+// export const videos = (req,res) => res.send('Videos');
 export const upload = (req,res) => res.send('Upload');
 export const videoDetail = (req,res) => res.send('Video Detail');
 export const editVideo = (req,res) => res.send('Edit Video');
 export const deleteVideo = (req,res) => res.send('Delete Video');
+	
